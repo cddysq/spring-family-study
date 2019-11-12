b232d25a5a7638d0c805a5ca4fe9e515d4c803f7
Rainbow Cat <2056423011@qq.com>
2019-11-12 20:25:31 +0800

    注释说明

--- hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author :Haotian
+ * @Parameter : @SpringBootApplication 是一个组合注解，用于快捷配置启动类,同于@Configuration+@EnableAutoConfiguration+@ComponentScan的合集
+ * @Parameter : @RestController 相当于@Controller + @ResponseBody 的作用结合,将controller的方法返回的对象通过适当的转换器转换为指定的格式之后，写入到response对象的body区
  */
 @SpringBootApplication
 @RestController
@@ -17,9 +19,13 @@ public class HelloSpringApplication {
         SpringApplication.run( HelloSpringApplication.class, args );
     }
 
+    /**
+     * @return 向请求(http://localhost:8080/hello)返回 Hello spring
+     * @Parameter : @RequestMapping 用于建立请求URL和处理请求方法之间的对应关系
+     */
     @RequestMapping("/hello")
     public String hello() {
-        //配置访问路径，返回指定数据
+        //指定返回数据
         return "Hello Spring";
     }
 }
