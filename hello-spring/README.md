291d94961f1b602e93d5d56425a6ccbe888915a5
Rainbow Cat <2056423011@qq.com>
2019-12-12 20:23:23 +0800

    注释更新

--- hello-spring/README.md
+++ hello-spring/README.md
@@ -1,8 +1,8 @@
 ## Spring-Boot 的第一个程序，当然是跑个 Hello-World
 
-**本次演示首次出现注解如下：**
+#### 本次演示首次出现注解如下：
 
-1. `@SpringBootApplication`
-2. `@RestController`
-3. `@RequestMapping`
+1. `@SpringBootApplication`：是一个组合注解，用于快捷配置启动类,等同于`@Configuration`+`@EnableAutoConfiguration`+`@ComponentScan`的合集。
+2. `@RestController`：相当于`@Controller` + `@ResponseBody` 的作用结合,将controller的方法返回的对象通过适当的转换器转换为指定的格式之后，写入到response对象的body区。
+3. `@RequestMapping`：用于建立请求URL和处理请求方法之间的对应关系。
 
--- hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Author :Haotian
- * @Parameter : @SpringBootApplication 是一个组合注解，用于快捷配置启动类,同于@Configuration+@EnableAutoConfiguration+@ComponentScan的合集
- * @Parameter : @RestController 相当于@Controller + @ResponseBody 的作用结合,将controller的方法返回的对象通过适当的转换器转换为指定的格式之后，写入到response对象的body区
- */
+ * @Author: Haotian
+ * @Date: 2019/12/12 20:20
+ * @Description: Spring-Boot first demo
+ **/
 @SpringBootApplication
 @RestController
 public class HelloSpringApplication {
@@ -21,7 +21,6 @@ public class HelloSpringApplication {
 
     /**
      * @return 向请求(http://localhost:8080/hello)返回 Hello spring
-     * @Parameter : @RequestMapping 用于建立请求URL和处理请求方法之间的对应关系
      */
     @RequestMapping("/hello")
     public String hello() {
