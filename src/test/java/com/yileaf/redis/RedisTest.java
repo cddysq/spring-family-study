43c8de8865c00c64f6cf948397c18aa954e345d7
Rainbow Cat <2056423011@qq.com>
2020-01-13 10:58:56 +0800

    Spring Boot study

--- src/test/java/com/yileaf/redis/RedisTest.java
+++ src/test/java/com/yileaf/redis/RedisTest.java
@@ -21,9 +21,32 @@ public class RedisTest {
 
     @Test
     public void demo() {
-        //string字符串
-        redisTemplate.boundValueOps( "name" ).set( "" );
-        log.info( "name={}", redisTemplate.opsForValue().get( "name" ));
+        //String字符串
+        redisTemplate.boundValueOps( "name" ).set( "雨宫美沙子" );
+        log.info( "String name={}", redisTemplate.opsForValue().get( "name" ) );
+
+        //Hash散列
+        redisTemplate.boundHashOps( "h_key" ).put( "name", "椎名真白" );
+        redisTemplate.boundHashOps( "h_key" ).put( "age", "18" );
+        //获取所有域对应的值
+        log.info( "hash散列所有的域：{}", redisTemplate.boundHashOps( "h_key" ).keys() );
+        log.info( "hash散列所有的域值：{}", redisTemplate.boundHashOps( "h_key" ).values() );
+
+        //List列表
+        redisTemplate.boundListOps( "l_key" ).leftPush( "c" );
+        redisTemplate.boundListOps( "l_key" ).leftPush( "b" );
+        redisTemplate.boundListOps( "l_key" ).leftPush( "a" );
+        log.info( "list列表的值：{}", redisTemplate.boundListOps( "l_key" ).range( 0, -1 ) );
+
+        //Set集合
+        redisTemplate.boundSetOps( "set_key" ).add( "a", "b", "c" );
+        log.info( "set集合的所有元素：{}", redisTemplate.boundSetOps( "set_key" ).members() );
+
+        //Sorted set有序集合
+        redisTemplate.boundZSetOps( "z_key" ).add( "a", 30 );
+        redisTemplate.boundZSetOps( "z_key" ).add( "b", 20 );
+        redisTemplate.boundZSetOps( "z_key" ).add( "c", 10 );
+        log.info( "有序set集合的所有元素：{}", redisTemplate.boundZSetOps( "z_key" ).range( 0, -1 ));
     }
 
 }
