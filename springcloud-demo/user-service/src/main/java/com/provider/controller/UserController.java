0e50469ae126684eeb04c0537b0ebb5220135afb
Rainbow Cat <2056423011@qq.com>
2020-01-16 21:41:35 +0800

    借助 Spring Cloud bus 进行配置修改刷新

--- springcloud-demo/user-service/src/main/java/com/provider/controller/UserController.java
+++ springcloud-demo/user-service/src/main/java/com/provider/controller/UserController.java
@@ -3,6 +3,7 @@ package com.provider.controller;
 import com.provider.pojo.Girl;
 import com.provider.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
+@RefreshScope //刷新配置
 public class UserController {
     @Autowired
     private UserService userService;
