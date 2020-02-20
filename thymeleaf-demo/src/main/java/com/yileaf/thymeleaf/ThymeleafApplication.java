c4b9db9249cb545aafbf266ce28e8699540c130d
Rainbow Cat <2056423011@qq.com>
2020-02-20 21:57:02 +0800

    搭建thymeleaf演示项目

---
+++ thymeleaf-demo/src/main/java/com/yileaf/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,17 @@
+package com.yileaf.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+* @Author: Haotian
+* @Date: 2020/2/20 21:54
+* @Description: 启动类
+**/
+@SpringBootApplication
+public class ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ThymeleafApplication.class, args );
+    }
+
+}
