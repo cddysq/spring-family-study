f65905216fce29de43d1c925541dfdf27c65e5ff
Rainbow Cat <2056423011@qq.com>
2020-04-25 21:20:08 +0800

    update

--- security-demo/basic-service/src/main/java/com/spring/security/demo/BasicServerApplication.java
+++ security-demo/basic-service/src/main/java/com/spring/security/demo/BasicServerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 启动类
+ *
  * @author Haotian
  * @version 1.0.0
  * @date 2020/4/23 17:52
