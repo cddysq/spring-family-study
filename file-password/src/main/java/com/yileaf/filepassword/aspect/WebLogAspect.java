5275953f5e41576b8567608bb3d3db853c0f8086
Rainbow Cat <2056423011@qq.com>
2020-07-05 20:12:24 +0800

    file passsword update

---
+++ file-password/src/main/java/com/yileaf/filepassword/aspect/WebLogAspect.java
@@ -0,0 +1,16 @@
+package com.yileaf.filepassword.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 接口访问日志记录
+ *
+ * @author Haotian
+ * @version 1.0.0
+ * @date 2020/7/5 20:07
+ */
+@Aspect
+@Component
+public class WebLogAspect {
+}
