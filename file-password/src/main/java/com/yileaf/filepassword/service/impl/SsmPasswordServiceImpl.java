989a440cffd5f89aec637e2ad4f2d162ff93c834
Rainbow Cat <2056423011@qq.com>
2020-07-10 23:56:22 +0800

    update

--- file-password/src/main/java/com/yileaf/filepassword/service/impl/SsmPasswordServiceImpl.java
+++ file-password/src/main/java/com/yileaf/filepassword/service/impl/SsmPasswordServiceImpl.java
@@ -1,12 +1,15 @@
 package com.yileaf.filepassword.service.impl;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.MD5;
 import com.yileaf.filepassword.config.SystemParams;
 import com.yileaf.filepassword.model.Ssm;
 import com.yileaf.filepassword.service.SsmPasswordService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author Haotian
@@ -22,7 +25,8 @@ public class SsmPasswordServiceImpl implements SsmPasswordService {
     public boolean getSsmPassword(Ssm ssm) {
         // 24进制转换后的群号
         String loginName = Integer.toString( Convert.toInt( systemParams.getLoginUsername() ), 24 );
-
+        String password = ssm.getPassword();
+        SecureUtil.md5( password + loginName );
         return false;
     }
 }
