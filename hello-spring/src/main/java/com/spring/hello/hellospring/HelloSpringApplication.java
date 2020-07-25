429632de8194b285097646f4e49ccf102467dfe2
Rainbow Cat <2056423011@qq.com>
2020-07-25 18:19:58 +0800

    update doc

--- hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/com/spring/hello/hellospring/HelloSpringApplication.java
@@ -6,25 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Author: Haotian
- * @Date: 2019/12/12 20:20
- * @Description: Spring-Boot first demo
+ * Spring-Boot first demo
+ *
+ * @author Haotian
+ * @version 1.0.0
+ * @date 2020/7/25 18:19
  **/
 @SpringBootApplication
 @RestController
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
-        //Spring-boot启动入口
+        // Spring-boot启动入口
         SpringApplication.run( HelloSpringApplication.class, args );
     }
 
     /**
-     * @return 向请求(http://localhost:8080/hello)返回 Hello spring
+     * @return 向请求(http : / / localhost : 8080 / hello)返回 Hello spring
      */
     @RequestMapping("/hello")
     public String hello() {
-        //指定返回数据
+        // 指定返回数据
         return "Hello World from the Spring";
     }
 }
